bbd1763e43fa389fc276683eb79616f2e34dabbf
morgana <89588301+morgana-x@users.noreply.github.com>
2024-09-26 01:29:45 +1000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,16 +6,30 @@ public partial class Program
     private static string ReadString(Process proc, long os)
     {
         List<byte> toCompile = new List<byte>();
+        int numC = 0;
         while (true)
         { 
             byte c = MemoryRead.ReadByte((int)proc.Handle, os);
-            if (c == 0x0)
-                break;
-            toCompile.Add(c);
             os++;
-        }
+            if (c != 0x0)
+            {
+                toCompile.Add(c);
+                numC = 0;
+                continue;
+            }
+            numC++;
+            if (numC >= 1)
+                break;
+            }
         return Encoding.Default.GetString(toCompile.ToArray());
     }
+    private class flowscriptFuncEntry
+    {
+        public string name;
+        public long entryOffset;
+        public long functionOffset;
+    }
+
     public static void Main(string[] args)
     {
         // See https://aka.ms/new-console-template for more information
@@ -25,7 +39,7 @@ public partial class Program
         Process proc = Process.GetProcessesByName("P5R")[0];
 
         long tableStart;
-        Dictionary<string, long> functionDump = new Dictionary<string, long>();
+        Dictionary<string, flowscriptFuncEntry> functionDump = new Dictionary<string, flowscriptFuncEntry>();
 
         long offset = 0x14192e890;
 
@@ -40,7 +54,9 @@ public partial class Program
             string funcName = stringPointer != 0 ? ReadString(proc,stringPointer) : "UNKNOWN";
             if (!funcName.ToLower().Contains(searchTerm))
                 continue;
-            functionDump.Add(funcName, funcPointer);
+            if (functionDump.ContainsKey(funcName))
+                funcName += "(1)";
+            functionDump.Add(funcName, new flowscriptFuncEntry() { name = funcName, entryOffset = offset-8, functionOffset = funcPointer});
             Console.WriteLine($"{funcName} : 0x{funcPointer.ToString("X")}");
         }
         string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
@@ -48,7 +64,7 @@ public partial class Program
         string dumpPath = Path.Combine(exeFolder, "flowscriptdump.txt");
         string text = "";
         foreach (var a in functionDump)
-            text += $"{a.Key} : 0x{a.Value.ToString("X")}\n";
+            text += $"{a.Key} : entOffset=0x{a.Value.entryOffset.ToString("X")} : func=0x{a.Value.functionOffset.ToString("X")}\n";
         File.WriteAllText(dumpPath, text);
     }
 }
